6e96fe3fa82d1f70df54f3ca4a719303146d983a
Csonic90 <sylwester.fron@gmail.com>
2022-01-02 18:40:57 +0100

    add  game iteration

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Image from 'next/image'
 import styles from '../styles/Test.module.css'
 
-export const Test = ({playersList}) => {
 
-console.log(playersList)
+export const Test = ({playersList,playersListInfo}) => {
 
- 
-
+console.log(playersListInfo)
+  if (!playersList) return <div>loading...</div>
   return (
     <div className={styles.container}>
       <h1 className={styles.header}>COLD HAND PLAYER</h1>
@@ -17,7 +16,8 @@ console.log(playersList)
         <div>{player.nickname}</div>
         <div><Image src={player.avatar} width={100} height={100}/></div>
       <div>ELO: {player.games.csgo.faceit_elo}</div> 
-      <div>LEVEL: {player.games.csgo.skill_level}</div>  
+      <div>LEVEL: {player.games.csgo.skill_level}</div> 
+      <div>Meczy:  {playersListInfo.find(e => e.player_id == player.player_id).lifetime.Matches} </div>
       </div>
      )}
      </div>
@@ -25,13 +25,19 @@ console.log(playersList)
   )
 }
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const players = ["0a7cdac8-b807-4db0-b371-246061b8d23d","a9de04f0-91cb-49ae-8872-97156a48406e","1911409b-0292-4f50-b66b-1dee3f9b7bd0", "96786b1a-c278-4efb-8123-c0a7fdd6fa63","eb1de30f-3c7e-48c7-9649-a3fea9e46558","1f60cb61-72fa-4fcf-b553-bb48e0c60343", "9bd84ca3-5e44-444e-ae6d-3dc1b59f99dc", "3cb52aca-91ab-4f09-9d9d-9d2f97701c2c" ]
 
     const playersList = await Promise.all(
       players.map(getPlayer)
     ); 
-   async function getPlayer(item, index){
+
+    
+    const playersListInfo = await Promise.all(
+      players.map(getPlayerInfo)
+    ); 
+    
+   async function getPlayer(item){
       
       const res = await fetch(`https://open.faceit.com/data/v4/players/${item}`, {
                                 method: "GET",
@@ -40,12 +46,28 @@ export const getStaticProps = async () => {
                                   "Authorization" : "Bearer 7ba74f51-8955-4749-9d5b-18c5e1fd8e72"
                                         }
                               });
-
           const data = await res.json();
-          return (index,data);
-                            }             
+          return (data);
+                            }
+          
+      async function getPlayerInfo(item){
+      
+        const resp = await fetch(`https://open.faceit.com/data/v4/players/${item}/stats/csgo`,{
+                                  method: "GET",
+                                  headers: {
+                                    "Content-type": "application/json;charset=UTF-8",
+                                    "Authorization" : "Bearer 7ba74f51-8955-4749-9d5b-18c5e1fd8e72"
+                                          }
+                                });
+                        
+                                 
+      const info = await resp.json();
+      return (info);
+        }
+                            
+                            
     return {
-        props: {playersList}
+        props: {playersList,playersListInfo}
           }
 
 }
